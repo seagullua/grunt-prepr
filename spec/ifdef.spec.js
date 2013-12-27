25a2e4afdf09cd2ec7568784563ebd80626cef2d
anton <anton.al.ivanov@gmail.com>
2013-12-27 16:25:13 +0400

    Implemented first two tests and initial support for the #ifdef directive

--- spec/ifdef.spec.js
+++ spec/ifdef.spec.js
@@ -1,9 +1,31 @@
 var prepr = (require ? require("../src/prepr") : this.prepr);
 
-describe("A suite", function() {
-  it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
+describe("#ifdef directive", function() {
+  it("processes single defined variable", function() {
+    var input = "#ifdef DEBUG\n\
+console.log('In the debug mode');\n\
+#endif\n\
+var x = 1;\n\
+#ifdef PROD\n\
+var y = x + 1;\n\
+#endif";
+    var expected = "var x = 1;\n\
+var y = x + 1;";
 
-    prepr.preprocess();
+    expect(prepr.preprocess(input, ["PROD"])).toBe(expected);
   });
+  
+  it("leaves input without directives intact", function() {
+      var input = "var x = 78;\n\
+var z = x * 2;";
+      var expected = "var x = 78;\n\
+var z = x * 2;";
+
+      expect(prepr.preprocess(input, ["PROD"])).toBe(expected);
+  });
+
+  //TODO: Several defines
+  //TODO: Windows line endings
+  //TODO: Different line endings on each line
+  //TODO: Possible to specify which line endings to use in output
 });
--- src/prepr.js
+++ src/prepr.js
@@ -1,8 +1,35 @@
 (function(host) {
     
     var exported = {
-        preprocess: function() {
-            console.log("Preprocessing");
+        preprocess: function(input, defines) {
+            var lines = input.split(/\r?\n/);
+            var ignoreLines = false;
+            var output = "";
+
+            defines = defines.map(function(str) { 
+                return str.toUpperCase();
+            });
+            for (var i = 0; i < lines.length; i++) {
+                var line = lines[i];
+                var isStartDirective = /#ifdef +(\S*)?/.exec(line);
+                var isEndDirective = /#endif/.exec(line);
+                var isDirective = isStartDirective || isEndDirective;
+
+                if (isStartDirective) {
+                    var mode = isStartDirective[1];
+
+                    if (defines.indexOf(mode.toUpperCase()) < 0) {
+                        ignoreLines = true;
+                    }
+                } 
+                if (!ignoreLines && !isDirective) {
+                    output = output + line + "\n";
+                }
+                if (isEndDirective) {
+                    ignoreLines = false;
+                }
+            };
+            return output.trim();
         }
     };
 
