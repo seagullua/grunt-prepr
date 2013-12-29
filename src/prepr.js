03de51ebe979bbeefb76bfe22b617f64f8e2f8f2
anton <anton.al.ivanov@gmail.com>
2013-12-29 12:10:25 +0400

    Re-factored the implementation so that it can run asynchronously by multiple clients at the same time (which was the case when running the Jasmine specs in the Node environment) and even concurrently.

--- src/prepr.js
+++ src/prepr.js
@@ -1,24 +1,12 @@
 (function(host) {
 
-    var outputLineSeparator = "\n",
-        directivesStack = [];
-
-    function getOutputLineSeparator(input) {
-        return input.indexOf("\r\n") >= 0 ? "\r\n" : "\n";
-    }
-
-    function getDirectiveStackRepresentation() {
-        return directivesStack.map(function(directive) {
-            return directive.definition;
-        }).join(",");
-    }
-
-    function IfDirective(definition, definedVariables, parentIfDirective) {
+    function IfDirective(definition, definedVariables, outputLineSeparator, parentIfDirective) {
         var directiveMatch = /#ifdef +(\S*)?/.exec(definition);
 
         this.definition = definition;
         this.variableName = directiveMatch[1].toUpperCase();
         this.ignoreContent = (definedVariables.indexOf(this.variableName) < 0);
+        this.outputLineSeparator = outputLineSeparator;
 
         if (parentIfDirective) {
             this.ignoreContent = this.ignoreContent || parentIfDirective.ignoreContent;
@@ -26,46 +14,61 @@
     }
 
     IfDirective.prototype.processLine = function(line) {
-        return this.ignoreContent ? "" : line + outputLineSeparator;
+        return this.ignoreContent ? "" : line + this.outputLineSeparator;
+    };
+
+    function Preprocessor(input, definedVariables) {
+        this.input = input;
+
+        if (typeof definedVariables == "string") {
+            definedVariables = [definedVariables];
+        }
+        this.definedVariables = definedVariables.map(function(str) { 
+            return str.toUpperCase();
+        });
+
+        this.outputLineSeparator = input.indexOf("\r\n") >= 0 ? "\r\n" : "\n",
+        this.directivesStack = [];
+    }
+    
+    Preprocessor.prototype.run = function() {
+        var lines = this.input.split(/\r?\n/),
+            output = "";
+
+        for (var i = 0; i < lines.length; i++) {
+            var line = lines[i],
+                topIfDirective = this.directivesStack[this.directivesStack.length - 1];
+
+            if (/#ifdef/.exec(line)) {
+                this.directivesStack.push(new IfDirective(line, this.definedVariables, this.outputLineSeparator, topIfDirective));
+            } else if (/#endif/.exec(line)) {
+                if (this.directivesStack.length == 0) {
+                    throw new Error("Found #endif without opening directive");
+                }
+                this.directivesStack.pop();
+            } else {
+                line = topIfDirective ? topIfDirective.processLine(line) : line + this.outputLineSeparator;
+
+                output = output + line;
+            }
+        };
+
+        if (this.directivesStack.length > 0) {
+            throw new Error("Unclosed if directives found " + this.getDirectiveStackRepresentation());
+        };
+
+        return output.substring(0, output.length - this.outputLineSeparator.length);
+    };
+    
+    Preprocessor.prototype.getDirectiveStackRepresentation = function() {
+        return this.directivesStack.map(function(directive) {
+            return directive.definition;
+        }).join(",");
     };
 
     var exported = {
         preprocess: function(input, definedVariables) {
-            var lines = input.split(/\r?\n/),
-                output = "";
-
-            if (typeof definedVariables == "string") {
-                definedVariables = [definedVariables];
-            }
-            
-            outputLineSeparator = getOutputLineSeparator(input);
-
-            definedVariables = definedVariables.map(function(str) { 
-                return str.toUpperCase();
-            });
-            for (var i = 0; i < lines.length; i++) {
-                var line = lines[i],
-                    topIfDirective = directivesStack[directivesStack.length - 1];
-
-                if (/#ifdef/.exec(line)) {
-                    directivesStack.push(new IfDirective(line, definedVariables, topIfDirective));
-                } else if (/#endif/.exec(line)) {
-                    if (directivesStack.length == 0) {
-                        throw new Error("Found #endif without opening directive");
-                    }
-                    directivesStack.pop();
-                } else {
-                    line = topIfDirective ? topIfDirective.processLine(line) : line + outputLineSeparator;
-
-                    output = output + line;
-                }
-            };
-
-            if (directivesStack.length > 0) {
-                throw new Error("Unclosed if directives found " + getDirectiveStackRepresentation());
-            };
-
-            return output.substring(0, output.length - outputLineSeparator.length);
+            return new Preprocessor(input, definedVariables).run();
         }
     };
 
