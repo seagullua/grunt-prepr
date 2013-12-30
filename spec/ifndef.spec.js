5da17ee5cad2506cf00b87b4768d26ec61a076ef
anton <anton.al.ivanov@gmail.com>
2013-12-30 17:10:18 +0400

    Added more tests for the ifndef directive

--- spec/ifndef.spec.js
+++ spec/ifndef.spec.js
@@ -15,90 +15,86 @@ var x = 1;";
 
         expect(prepr.preprocess(input, ["PROD"])).toBe(expected);
     });
-/*
+
     it("processes several defined variables", function() {
-        var input = "#ifdef var1\n\
+        var input = "#ifndef var1\n\
 line1\n\
 #endif\n\
-#ifdef var2\n\
+#ifndef var2\n\
 line2\n\
 #endif\n\
-#ifdef var3\n\
+#ifndef var3\n\
 line3\n\
 #endif\n\
-#ifdef var4\n\
+#ifndef var4\n\
 line4\n\
 #endif\n\
-#ifdef var5\n\
+#ifndef var5\n\
 line5\n\
 #endif";
-        var expected = "line1\n\
-line3\n\
-line5";
+        var expected = "line2\n\
+line4";
 
         expect(prepr.preprocess(input, ["var1", "var3", "var5"])).toBe(expected);
     });
 
     it("considers variables case insensitive", function() {
-        var input = "#ifdef var1\n\
+        var input = "#ifndef var1\n\
 line1\n\
 #endif\n\
-#ifdef VAR1\n\
+#ifndef VAR1\n\
 line2\n\
 #endif\n\
-#ifdef var2\n\
+#ifndef var2\n\
 line3\n\
 #endif";
-        var expected = "line1\n\
-line2";
+        var expected = "line3";
 
         expect(prepr.preprocess(input, ["var1"])).toBe(expected);
         expect(prepr.preprocess(input, ["VAR1"])).toBe(expected);
     });
 
     it("ignores leading spaces before directive", function() {
-        var input = "  #ifdef var1\n\
+        var input = "  #ifndef var1\n\
   line1\n\
   #endif\n\
-    #ifdef var1\n\
+    #ifndef var1\n\
     line2\n\
   #endif\n\
- #ifdef var2\n\
+ #ifndef var2\n\
 line3\n\
     #endif";
-        var expected = "  line1\n\
-    line2";
+        var expected = "line3";
 
         expect(prepr.preprocess(input, ["var1"])).toBe(expected);
     });
 
     it("processes nested directives, one active one non-active", function() {
-        var input = "#ifdef var1\n\
+        var input = "#ifndef var1\n\
 line1\n\
-#ifdef var2\n\
+#ifndef var2\n\
 line2\n\
 #endif\n\
 line3\n\
 #endif\n\
-#ifdef var2\n\
+#ifndef var2\n\
 line4\n\
-#ifdef var1\n\
+#ifndef var1\n\
 line5\n\
 #endif\n\
 line6\n\
 #endif";
-        var expected = "line4\n\
-line6";
+        var expected = "line1\n\
+line3";
 
         expect(prepr.preprocess(input, ["var2"])).toBe(expected);
     });
-
     it("processes active nested directives", function() {
-        var input = "#ifdef var1\n\
+        var input = "#ifndef var1\n\
 line1\n\
-#ifdef var2\n\
+#ifndef var2\n\
 line2\n\
-#ifdef var3\n\
+#ifndef var3\n\
 line3\n\
 #endif\n\
 line4\n\
@@ -111,16 +107,15 @@ line3\n\
 line4\n\
 line5";
 
-        expect(prepr.preprocess(input, ["var1", "var2", "var3"])).toBe(expected);
+        expect(prepr.preprocess(input, ["var4", "var5"])).toBe(expected);
     });
 
     it("raises error when not closed", function() {
-        var input = "#ifdef var1\n\
+        var input = "#ifndef var1\n\
 line1";
 
         expect(function() {
             prepr.preprocess(input, ["var1"]);
-        }).toThrow(new Error("Unclosed if directives found #ifdef var1"));
+        }).toThrow(new Error("Unclosed if directives found #ifndef var1"));
     });
-*/
 });
