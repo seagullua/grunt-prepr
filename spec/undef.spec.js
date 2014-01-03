18e04140ee52267db4291ee03020582f32a836c6
anton <anton.al.ivanov@gmail.com>
2014-01-03 11:39:51 +0400

    Added tests for the #undef directive, which will be implemented in the future revisions.

---
+++ spec/undef.spec.js
@@ -0,0 +1,39 @@
+var prepr = (require ? require("../src/prepr") : this.prepr);
+
+describe("#undef directive", function() {
+
+    it("can be used to undefine a predefined variable", function() {
+        var input = "#ifdef var1\n\
+line1\n\
+#endif\n\
+#undef var1\n\
+#ifdef var1\n\
+line2\n\
+#endif";
+        var expected = "line1";
+
+        expect(prepr.preprocess(input, ["var1"])).toBe(expected);
+    });
+    
+    it("does not raise exception with a variable that was not defined", function() {
+        var input = "#undef var1\n\
+line1";
+        var expected = "line1";
+
+        expect(prepr.preprocess(input)).toBe(expected);
+    });
+    
+    it("can be used to undefine a variable defined with #define", function() {
+        var input = "#define var1\n\
+#ifdef var1\n\
+line1\n\
+#endif\n\
+#undef var1\n\
+#ifdef var1\n\
+line2\n\
+#endif";
+        var expected = "line1";
+
+        expect(prepr.preprocess(input)).toBe(expected);
+    });
+});
